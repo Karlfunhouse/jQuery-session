c13c1e5f24e406e05f74f872c22f30d5749334b5
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-13 11:14:26 -0700

    Did the exercises

--- part-2-content/content.js
+++ part-2-content/content.js
@@ -1,4 +1,3 @@
-const $ = require('jquery');
 let btn = document.querySelector('button');
 btn.addEventListener('click', function(e){
   e.preventDefault();
@@ -12,3 +11,21 @@ clearBtn.addEventListener('click', function(){
 })
 
 console.log($('p').text())
+
+
+// Within the JS file, use jQuery to:
+// 1. Modify the inner text of the 'Save Contact' button to say "Add New Buddy"
+
+$("button[type='submit']").text('Add New Buddy')
+// 2. Make the check box labels more enthusiastic (ex: `Yes!!!` or `No!!!`)
+
+$("label[for='confirm']").text('Heavens Yes!')
+$("label[for='deny']").text('Hell No!')
+// 3. Give all of the inputs default values when the page loads (not placeholders -- actual values)
+$("input").val('SUP HOMIE?')
+$(".phone").val('111')
+
+// 4. Select both headings (h1 and h2) and change them to say "Contact Form".
+
+$("h1, h2").text("Contact Form")
+// 5. Commit your changes!
