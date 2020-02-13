5715a62713de9e29f68c9a20c4e342b62e5d362d
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-13 10:47:01 -0700

    changed some colors

--- part-1-selectors/selectors.js
+++ part-1-selectors/selectors.js
@@ -9,3 +9,24 @@ clearBtn.addEventListener('click', function(){
     input.value = "";
   })
 })
+
+// 1. Select all the labels and give them a class of "green"
+$('label').addClass('green')
+
+// 2. Select all of the `phone` inputs and give them a class of "yellow"
+$(".phone").addClass("yellow")
+
+// 3. Select the label for the email input and give it a class of "blue"
+$("label[for='email']").addClass('blue').removeClass('green')
+
+// 4. On page load, log the id of the checked check box (hint: look into the `.attr()` jQuery method)
+
+const checkedID = $("input[type='checkbox']:checked").attr("id")
+console.log(checkedID);
+
+// 5. Give all labels inside of a p tag a class of "purple"
+
+$("p").find("label").addClass("purple")
+// 6. Give the Clear button a class of red
+
+$("#clear-btn").addClass('red')
