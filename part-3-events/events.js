c15d58f9fad6dec1fd4829957e05bbfe63da46a9
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-17 12:45:22 -0700

    Did some exercises

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -1,14 +1,36 @@
-const $ = require('jquery');
-let btn = document.querySelector('button');
-btn.addEventListener('click', function(e){
-  e.preventDefault();
+// const $ = require('jquery');
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function(e){
+//   e.preventDefault();
+// })
+
+// let clearBtn = document.querySelector('#clear-btn');
+// clearBtn.addEventListener('click', function(){
+//   document.querySelectorAll('input').forEach(input => {
+//     input.value = "";
+//   })
+// })
+
+console.log($('p').text())
+
+
+// In the JS file:
+// 1. Rewrite the event handlers on each of the buttons into jQuery code.
+${'button'}.on('click', function(e) {
+  e.preventDefault()
 })
 
-let clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', function(){
+${'#clear-btn'}.on('click', function(){
   document.querySelectorAll('input').forEach(input => {
     input.value = "";
   })
 })
 
-console.log($('p').text())
+// 2. Listen for an `input` event on all inputs on the page. When the event fires, save that input's value to local storage
+
+  ${'input'}.on('change', )
+
+// 3. When the page loads, set the value of the inputs to what was saved in local storage
+
+
+// 4. When you click the clear button, clear local storage as well.
